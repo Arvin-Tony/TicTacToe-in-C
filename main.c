fb75ee2656f800b1e2e8e076fbcd388b761676b2
Arvin-Tony <124749409+Arvin-Tony@users.noreply.github.com>
2023-05-17 00:44:04 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 
 // Input-Output based Tic-Tac-Toe Game. Instructions print when run.
-
+// If providing an input file to the program, 
 
 // check_arr(arr, x_or_o) checkis if the array is filled with
 //   the same character
@@ -28,7 +28,8 @@ bool check_arr(char arr[], char x_or_o) {
 //   takes in the the arrays representing the board and checks if 
 //   a player has won
 // Time: O(n)
-bool check_win(char x_or_o, char* arr_all_horiz[], char* arr_all_vert[], char arr_diag_1[], char arr_diag_2[]) {
+bool check_win(char x_or_o, char* arr_all_horiz[], char* arr_all_vert[], 
+                                        char arr_diag_1[], char arr_diag_2[]) {
     assert(arr_all_horiz);
     assert(arr_all_vert);
     assert(arr_diag_1);
@@ -59,7 +60,8 @@ bool check_win(char x_or_o, char* arr_all_horiz[], char* arr_all_vert[], char ar
 // update_diag(arr_diag_1, arr_diag_2, row, col, x_or_o) updates the 
 //   respective diagonal arrays to reflect the recent play
 // effects: may modify arr_diag_1 and/or arr_diag_2
-void update_diag(char arr_diag_1[], char arr_diag_2[], int row, int col, char x_or_o) {
+void update_diag(char arr_diag_1[], char arr_diag_2[], 
+                            int row, int col, char x_or_o) {
     assert(arr_diag_1);
     assert(arr_diag_2);
     assert(1 <= row && row <= 3);
@@ -92,7 +94,8 @@ void update_vert(char arr_vert[], int row, char x_or_o) {
 
 // update_horiz(arr_horiz, col, x_or_o) updates the correct horiztonal array
 //   (the row) as necessary based on the recent play. It returns true if said
-//   play was valid (ie, doesn't overlapp another non-empty space), false otherwise.
+//   play was valid (ie, doesn't overlapp another non-empty space), 
+//   false otherwise.
 // Effects: may modifiy arr_horiz
 bool update_horiz(char arr_horiz[], int col, char x_or_o) {
     assert(arr_horiz);
@@ -146,9 +149,6 @@ int main() {
     
     bool player1 = true;
     
-    int row = 0;
-    int col = 0;
-    
     char arr_horiz_1[3] = {'_', '_', '_'};
     char arr_horiz_2[3] = {'_', '_', '_'};
     char arr_horiz_3[3] = {'_', '_', '_'};
@@ -170,12 +170,19 @@ int main() {
     bool one_win = false;
     bool two_win = false;
     
-    printf("Welcome to Tic Tac Toe!\nTo play the game, please enter the position you want to enter your\ncharacter in as two integers: row number column number (no commas or other characters!)\n");
-
-    printf("For example, when prompted, you enter: 1 1\nYour character will print at the top left corner of the board.\n");
-    printf("For example, when prompted, you enter: 1 3 \nYour character will print at the top right corner of the board.\n");
+    printf("Welcome to Tic Tac Toe!\n"); 
+    printf("To play the game, please enter the position you want to enter\n");
+    printf("your character in as two integers: row number, column number\n");
+    printf("For example, when prompted, you enter: (1, 1)\n");
+    printf("Your character will print at the top left corner of the board.\n");
+    printf("Note that commas and characters not neccessary, and\n");
+    printf("you can separate the two numbers by anything.\n");
+    printf("For example, when prompted, you enter: 1 3\n");
+    printf("Your character will print at the top right corner of the board.\n");
     printf("Player 1 is 'X' and Player 2 is 'O'.\n");
-    printf("After each turn, the current board will print.\n");
+    printf("If you try to place your character where a character already");
+    printf("exists\nyou will be prompted to enter another point with ");
+    printf("no demerit.\nAfter each turn, the current board will print.\n");
     printf("Here's the board right now:\n");
     print_board(arr_horiz_1, arr_horiz_2, arr_horiz_3);
     printf("Best of luck!\n");
@@ -184,9 +191,14 @@ int main() {
     
     bool first = true;
     
-    //char garbage = 'c';
+    char garbage = 'c';
+    
+    bool eof = false;
     
     while (true) {
+        int row = 0;
+        int col = 0;
+        
         if (first) {
             first = false;
         } else {
@@ -199,24 +211,39 @@ int main() {
             printf("Player 2 (O), enter coordinates:\n");
         }
 
-        // must check if two valid numbers provided
+        // getting two valid numbers at a time, and ensuring input exists
+        bool first_in = false;
+        bool second_in = false;
         
-        /* Will implement later - allowing for commas and other characters
-        if (scanf("%d", &row) != 1) {
-            printf("Invalid input. Enter a number please.\n");
-            scanf(" %c", &garbage);
-            continue;
+        while (eof == false) {
+            if (first_in == false) {
+                int retval_1 = scanf("%d", &row);
+                if (retval_1 == 1) {
+                    first_in = true;
+                } else if (retval_1 == 0) {
+                    int retval_2 = scanf(" %c", &garbage);
+                    if (retval_2 != 1) eof = true;
+                } else {
+                    eof = true;
+                }
+            } else if (second_in == false) {
+                int retval_1 = scanf("%d", &col);
+                if (retval_1 == 1) {
+                    second_in = true;
+                } else if (retval_1 == 0) {
+                    int retval_2 = scanf(" %c", &garbage);
+                    if (retval_2 != 1) eof = true;
+                } else {
+                    eof = true;
+                }
+            } else {
+                break;
+            }
+        } 
+        
+        if (row == 0 || col == 0) {
+            break;
         }
-        while (scanf("%d", &col) != 1) {
-            printf("Invalid input. Enter a second number as well please:\n");
-            scanf(" %c", &garbage);
-            
-        }
-        
-        */
-        
-        scanf("%d", &row);
-        scanf("%d", &col);
         
         if (row < 1 || row > 3) {
             printf("Invalid input. Please make sure your row is between 1 and 3\n");
@@ -235,7 +262,6 @@ int main() {
         }
         
         // update rows, columns, and diagonals
-        
         if (!update_horiz(arr_all_horiz[row - 1], col, x_or_o)) {
             printf("Letter placed at position already, try again:\n");
             continue;
@@ -262,6 +288,10 @@ int main() {
         }
         
         player1 = !player1;
+        
+        if (eof) {
+            break;
+        }
     }
     
     print_board(arr_horiz_1, arr_horiz_2, arr_horiz_3);
@@ -272,6 +302,10 @@ int main() {
         printf("Congratulations Player 2 (O)! You win!");
     } else if (table_full) {
         printf("Well played to both players. It's a tie!");
+    }
+    
+    if (eof) {
+        printf("You don't have enough input! Above is your final board.");
     }
     
     return 0;
